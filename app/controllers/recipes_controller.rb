3fecf29f13ef9297125e5407334fa81479fff063
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-27 16:03:06 -0400

    Create a recipe with ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,6 +33,6 @@ class RecipesController < ApplicationController
   private 
 
   def recipe_params 
-    params.require(:recipe).permit(:name)
+    params.require(:recipe).permit(:name, ingredient_ids: [])
   end
 end
