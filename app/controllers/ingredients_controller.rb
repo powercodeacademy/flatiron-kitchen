8d6822cfda6d273d9953fecb336d3cba6ecc56ab
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-27 15:32:36 -0400

    Edit ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,21 @@
 class IngredientsController < ApplicationController 
   def edit 
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def update 
+    @ingredient = Ingredient.find(params[:id])
+
+    if @ingredient.update(ingredient_params)
+      redirect_to @ingredient 
+    else 
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  private 
+
+  def ingredient_params 
+    params.require(:ingredient).permit(:name)
   end
 end
